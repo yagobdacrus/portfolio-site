ed356d5f4bddec0c815233dc275c72ba5eaf0366
Yago Bezerra <yagobdacruz@hotmail.com>
2025-06-09 21:05:22 -0300

    Voltando tudo desde inicio

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 class MobileNavbar {
+
   constructor(mobileMenu, navList, navLinks) {
     this.mobileMenu = document.querySelector(mobileMenu);
     this.navList = document.querySelector(navList);
@@ -6,6 +7,7 @@ class MobileNavbar {
     this.activeClass = "active";
 
     this.handleClick = this.handleClick.bind(this);
+
   }
 
   animateLinks() {
@@ -16,6 +18,7 @@ class MobileNavbar {
             index / 7 + 0.3
           }s`);
     });
+
   }
 
   handleClick() {
@@ -23,6 +26,8 @@ class MobileNavbar {
     this.mobileMenu.classList.toggle(this.activeClass);
     this.animateLinks();
   }
+
+
 
   addClickEvent() {
     this.mobileMenu.addEventListener("click", this.handleClick);
@@ -32,42 +37,57 @@ class MobileNavbar {
     if (this.mobileMenu) {
       this.addClickEvent();
     }
+
     return this;
   }
+
 }
 
 const mobileNavbar = new MobileNavbar(
+   
   ".mobile-menu",
+
   ".nav-list",
+
   ".nav-list li",
+
 );
+
 mobileNavbar.init();
+
+
 
 document.addEventListener("DOMContentLoaded", function () {
   const links = document.querySelectorAll(".nav-list a");
   const sections = document.querySelectorAll(".section, .hero");
-
   function showSection(id) {
-    sections.forEach((section) => {
+   sections.forEach((section) => {
       section.classList.remove("active");
+
     });
 
     const target = document.querySelector(id);
     if (target) {
       target.classList.add("active");
     }
+
   }
+
+
 
   links.forEach((link) => {
     link.addEventListener("click", (e) => {
       e.preventDefault();
       const targetId = link.getAttribute("href");
+
       showSection(targetId);
+
     });
+
   });
 
   // Mostra a seção "home" por padrão
+
   showSection("#home");
+
 });
-
-
